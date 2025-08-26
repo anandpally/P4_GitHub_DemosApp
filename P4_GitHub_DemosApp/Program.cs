221a00e737de560cb53c1476efb46d514a0833d8
parmanand <anand.pally123@gmail.com>
2025-08-26 11:42:08 +0530

    asdf

--- P4_GitHub_DemosApp/Program.cs
+++ P4_GitHub_DemosApp/Program.cs
@@ -10,7 +10,6 @@ namespace P4_GitHub_DemosApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
             Console.WriteLine("Welcome To GitHub account");
             Console.WriteLine("Welcome To Deloitte Consulting Private Limited");
             for (int i = 0; i < 10; i++)
